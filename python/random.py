9efe84f481321ba368d53f72528a3b4ee7b1fa3b
keisuke <keisukeseto.89103@gmail.com>
2024-11-04 19:20:18 +0900

    init:Python課題提出のため、グループ分け機能作成

--- python/random.py
+++ python/random.py
@@ -1,1 +1,10 @@
 
+import random
+
+members_list = ["A", "B", "C", "D", "E", "F"]
+random.shuffle(members_list)
+group_a_size = random.randrange(2, 4)
+group_a_members_list = sorted(members_list[:group_a_size])
+group_b_members_list = sorted(members_list[group_a_size:])
+print(group_a_members_list)
+print(group_b_members_list)
